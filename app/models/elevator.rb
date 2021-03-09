c932907028a0b419294a398532d38d2a6ea8c3b8
Claude St-Laurent <teambiti@yahoo.com>
2021-03-09 11:14:42 -0500

    before merge

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,6 +1,18 @@
-require 'slack-ruby-client'
+# require 'slack-ruby-client'
 require 'date'
 
 class Elevator < ApplicationRecord
     belongs_to :column ,optional: true
+    before_update :slack_message
+
+    def slack_message
+        timestamp = DateTime.now.strftime("%d-%m-%Y %H:%M")
+        e = Elevator.find(self.id)
+        text = "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.status_was} to #{self.status} at #{timestamp}."
+        Slack.configure do |config|
+            config.token = ENV['SLACK_WEBHOOK']
+        end
+        client = Slack::Web::Client.new
+        client.chat_postMessage(channel: '#elevator_operations', text: text, as_user: true)
+    end
 end
