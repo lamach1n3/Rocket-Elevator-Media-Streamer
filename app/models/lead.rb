6614a124d2f6f3c152b8b4c8e4e142dcddf34029
Claude St-Laurent <teambiti@yahoo.com>
2021-03-10 10:27:13 -0500

    Zendesk code finish not test

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,2 +1,27 @@
 class Lead < ApplicationRecord
+
+    after_save :new_zendesk_ticket_lead
+
+    def new_zendesk_ticket_lead
+        ZendeskAPI::Ticket.create!(client,
+        :subject => "#{self.full_name} from #{self.company_name}",
+        :comment => {
+            :value => "The contact #{self.full_name} from #{self.company_name} can be reach at email #{self.quotes_email}
+            and at phone number #{self.phone}.
+            #{self.department} has a project named #{self.project_name} which would require contribution from Rocket Elevators.
+
+            #{self.project_description}
+
+            Attached Message: #{self.message}
+
+            The Contact uploaded an attachment"
+        },
+        :requester =>{
+            "Name": self.full_name,
+            "Email": self.quotes_email
+        },
+        :priority => "normal",
+        :type => "question"
+        )
+    end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,24 @@
 class Quote < ApplicationRecord
+
+    after_save :new_zendesk_ticket_quote
+
+    def new_zendesk_ticket_quote
+        ZendeskAPI::Ticket.create!(client,
+        :subject => "#{self.quotes_name} from #{self.quotes_company_name}",
+        :comment => {
+            :value => "The contact #{self.quotes_name} from #{self.quotes_company_name} can be reach at this email: #{self.quotes_email}.
+            
+            The building type selected for the quote is #{self.building_type} with the product line #{self.product_line}.
+            The number of elevator suggested is #{self.number_of_elevators}, for a total price of #{self.total_price}.
+            
+            The Quote reference number is #{self.id}."
+        },
+        :requester =>{
+            "Name": self.quotes_name,
+            "Email": self.quotes_email
+        },
+        :priority => "normal",
+        :type => "task"
+        )
+    end
 end
