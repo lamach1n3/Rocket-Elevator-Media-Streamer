984859c159c7982c459ee92598d1ffad3d00d293
Claude St-Laurent <teambiti@yahoo.com>
2021-03-10 08:42:41 -0500

    comment add

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,11 +1,11 @@
-require 'date'
+require 'date'  #gem use to format timestamp at the end of the message
 
 class Elevator < ApplicationRecord
     belongs_to :column ,optional: true
     before_update :slack_message
 
     def slack_message
-        timestamp = DateTime.now.strftime("%d-%m-%Y %H:%M")
-        SlackNotifier::MESSAGE.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.status_was} to #{self.status} at #{timestamp}."
+        timestamp = DateTime.now.strftime("%d-%m-%Y %H:%M")         #to format timestamp at the end of the message
+        SlackNotifier::MESSAGE.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.status_was} to #{self.status} at #{timestamp}."    #message to Slack channel via Team-#4 app (WebHook)
     end
 end
