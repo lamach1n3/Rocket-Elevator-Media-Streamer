d485ad228115e040ce67ec223d176569a102b3e4
emmatshi-stack <tshibangubiayi@gmail.com>
2021-03-10 08:54:45 -0500

    mapbutton

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,5 @@
+require Rails.root.join('lib', 'rails_admin', 'gmap.rb')
+
 RailsAdmin.config do |config|
   ### Popular gems integration
   config.authorize_with do
@@ -47,6 +49,7 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
+    gmap
 
     ## With an audit adapter, you can add:
     # history_index
