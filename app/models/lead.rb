109dc27698beb2191fce78213e9ea112b3c1a656
Claude St-Laurent <teambiti@yahoo.com>
2021-03-10 13:56:15 -0500

    change in lead and quote models

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -6,7 +6,7 @@ class Lead < ApplicationRecord
         ZendeskAPI::Ticket.create!(client,
         :subject => "#{self.full_name} from #{self.company_name}",
         :comment => {
-            :value => "The contact #{self.full_name} from #{self.company_name} can be reach at email #{self.quotes_email}
+            :value => "The contact #{self.full_name} from #{self.company_name} can be reach at email #{self.email}
             and at phone number #{self.phone}.
             #{self.department} has a project named #{self.project_name} which would require contribution from Rocket Elevators.
 
@@ -18,7 +18,7 @@ class Lead < ApplicationRecord
         },
         :requester =>{
             "Name": self.full_name,
-            "Email": self.quotes_email
+            "Email": self.email
         },
         :priority => "normal",
         :type => "question"
