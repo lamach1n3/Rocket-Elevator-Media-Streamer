e595684a9682cdc2e2fb60a0059cdbfcf75f0c05
Claude St-Laurent <teambiti@yahoo.com>
2021-03-11 14:41:25 -0500

    branch main in production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,6 @@ before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "ClaudeStLaurent"
 set :repo_url, "https://github.com/Week-7-Team-4/Rocket_Elevators_API.git"
-set :branch, 'main'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
