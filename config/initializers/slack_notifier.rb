26cdaa805556f70ec1e0c446f64f86db618960b2
Claude St-Laurent <teambiti@yahoo.com>
2021-03-11 22:13:50 -0500

    slack url hardcode

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,4 +1,4 @@
 module SlackNotifier
-    MESSAGE = Slack::Notifier.new ENV["SLACK_WEBHOOK"]  #define MESSAGE for use in elevator controller
+    MESSAGE = Slack::Notifier.new "https://hooks.slack.com/services/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"  #define MESSAGE for use in elevator controller
 end
 
