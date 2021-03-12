5c35243d39a9276743b985b7557777d53c464d34
Claude St-Laurent <teambiti@yahoo.com>
2021-03-12 11:29:09 -0500

    verify token removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 require 'pg'
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
     # protect_from_forgery with: :exception
     
     
