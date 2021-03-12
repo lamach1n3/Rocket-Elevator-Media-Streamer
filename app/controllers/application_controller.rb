62c3918e5438d36acc72f846cddd28eac65fd751
Claude St-Laurent <teambiti@yahoo.com>
2021-03-12 14:19:18 -0500

    lines changed in appl.._control..rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require 'pg'
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
     skip_before_action :verify_authenticity_token
-    protect_from_forgery prepend: true, with: :exception
   
 end 
 
