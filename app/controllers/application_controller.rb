e9ad58d23d8f0af09a2c02b02a4eb11211f29d08
Claude St-Laurent <teambiti@yahoo.com>
2021-03-12 15:30:49 -0500

    app control with exeption removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 require 'pg'
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true, with: :exception  
+    protect_from_forgery prepend: true
     skip_before_action :verify_authenticity_token
   
 end 
