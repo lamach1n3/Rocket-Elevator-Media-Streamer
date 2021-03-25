d276098cf17c4365735ab30f0871eccb50c066b3
lamach1n3 <taillefer.jf@gmail.com>
2021-03-25 19:01:11 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.15.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "ClaudeStLaurent"
+set :application, "jft"
 set :repo_url, "https://github.com/lamach1n3/Rocket-Elevator-Foundation.git"
 # set :linked_files, %w{config/application.yml}
 # Default branch is :master
