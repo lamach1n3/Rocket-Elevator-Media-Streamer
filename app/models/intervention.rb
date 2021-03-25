d6734c67a5e71bb1783d8c73706ba1d71443a7cf
lamach1n3 <taillefer.jf@gmail.com>
2021-03-25 16:42:39 -0400

    deploy time

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -1,2 +1,9 @@
 class Intervention < ApplicationRecord
+    belongs_to :customer
+    belongs_to :building
+    belongs_to :battery, optional: true
+    belongs_to :column, optional: true
+    belongs_to :elevator, optional: true
+    belongs_to :employee, optional: true
+    belongs_to :author, class_name: "Employee"    #class name permit to link the author_id to a employee
 end
--- db/migrate/20210222234856_add_all_reference.rb
+++ db/migrate/20210222234856_add_all_reference.rb
@@ -16,5 +16,13 @@ class AddAllReference < ActiveRecord::Migration[5.2]
     add_reference :addresses,   :customer, foreign_key: true
     add_reference :customers,   :lead, foreign_key: true
 
+    add_reference :interventions, :customer, foreign_key: true
+    add_reference :interventions, :building, foreign_key: true
+    add_reference :interventions, :battery, foreign_key: true
+    add_reference :interventions, :column, foreign_key: true
+    add_reference :interventions, :elevator, foreign_key: true
+    add_reference :interventions, :employee, foreign_key: true
+    add_reference :interventions, :author, foreign_key: {to_table: :employees}
+
   end
 end
